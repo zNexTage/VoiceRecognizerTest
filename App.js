2b123676bd321976247a9962caec9fa1f8ca5611
christian_oliveira <christianoliveira1908@hotmail.com>
2020-10-10 17:54:11 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,14 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Voice from '@react-native-community/voice';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognized: '',
+      started: '',
+      results: [],
+    };
+
+
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+  }
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+  onSpeechStart(e) {
+    this.setState({
+      started: '√',
+    });
+  };
+
+  onSpeechRecognized(e) {
+
+    console.log("AQUI", e);
+
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechResults(e) {
+    console.log(e.value);
+    this.setState({
+      results: e.value,
+    });
+  }
+
+  async _startRecognition(e) {
+    this.setState({
+      recognized: '',
+      started: '',
+      results: [],
+    });
+    try {
+      await Voice.start('pt-BR');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.transcript}>
+            Minhas Falas:
+        </Text>
+          {this.state.results.map((result, index) => <Text key={index} style={styles.transcript}> {result}</Text>
+          )}
+        </View>
+        <View style={{flex: 2, justifyContent:'flex-end'}}>
+          <Button style={styles.transcript}
+            onPress={this._startRecognition.bind(this)}
+            title="APERTA AQUI"></Button>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +83,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  transcript: {
+    textAlign: 'center',
+    color: 'blue',
+    fontSize: 15
   },
 });
--- android/app/src/main/java/com/voicerecognizer/MainApplication.java
+++ android/app/src/main/java/com/voicerecognizer/MainApplication.java
@@ -6,6 +6,9 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.wenkesj.voice.VoicePackage;
+import com.wenkesj.voice.VoicePackage;
+import com.wenkesj.voice.VoicePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
